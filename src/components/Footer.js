fad79fd9b61d26f9d6478b7019eb376289fa5d0f
AngryDoggaebi <aofhoa@gmail.com>
2023-05-15 17:03:09 +0900

    header footer modal

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer () {
+  return (
+
+    <footer>
+      <p>개인정보 처리방침 | 이용약관</p>
+      <p>All rights reserved @ Codestates</p>
+    </footer>
+  )
+}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,54 @@
+import CozShopping from "../img/CozShopping"
+import Codestateslogo from "../img/Codestateslogo"
+import Hamberger from "../img/Hamberger"
+import Gift from "../img/Gift"
+import StarIcon from "../img/StarIcon"
+import { useState } from "react"
 
+
+
+
+export default function Header(){
+
+  let [modal, setModal] = useState(false);
+
+  return(
+    <>
+    <header>
+      <div id="logo_cozShopping">
+        <Codestateslogo/>  
+        <CozShopping/> 
+      </div>
+      
+      <div onClick={ () => { modal === true ? setModal(false) : setModal(true)} }>
+        <Hamberger/>
+      </div>
+      
+ 
+      
+    </header>
+    { modal === true ? <HamModal/> : null }
+    
+    </>
+  )
+
+}
+
+function HamModal () {
+  return (
+    <>
+    <div className='triangle'></div>
+    <div className="modal">
+      <div className="modalList">OOO님, 안녕하세요!</div>
+      <div className="division-line"></div>
+      <div className="modalList"><Gift/> 상품 리스트 페이지</div>
+      <div className="division-line"></div>
+      <div className="modalList"><StarIcon/> 북마크 페이지</div>
+    </div>
+    </>
+    
+
+
+
+  );
+}
