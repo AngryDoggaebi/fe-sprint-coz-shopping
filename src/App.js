05292464faed0c0f14aa5d00c6aa9d44fcb0e706
AngryDoggaebi <aofhoa@gmail.com>
2023-05-15 15:18:52 +0900

    “연습”

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+
+      <div id='wrapper'>
+        <div>
+          content
+        </div>
+      </div>
+
+      
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
