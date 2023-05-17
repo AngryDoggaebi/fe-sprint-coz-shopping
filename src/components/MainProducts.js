67f25d28d287187eb43d72011ee8a2e50b7664c1
AngryDoggaebi <aofhoa@gmail.com>
2023-05-17 14:25:11 +0900

    mainproduct 수정

--- src/components/MainProducts.js
+++ src/components/MainProducts.js
@@ -1,5 +1,5 @@
 import styled from "styled-components"
-import ItemCard from "./ItemCard";
+import ItemCard from "./ItemCard"
 
 let Section = styled.section`
   display: flex;
@@ -8,15 +8,20 @@ let Section = styled.section`
 `
 
 export default function MainProducts ({ products }) {
-  let newArr = products.filter(v => products.indexOf(v) < 4);
+
 
   return (
     <section id="wrapper">
       <button onClick={()=>{ console.log(products[1]) }}>버튼</button>
 
       <Section>
-      <ItemCard newArr={newArr}/>
+      <p>{ products[0].title }</p>
+      <p>{ products[1].title }</p>
+      <p>{ products[2].brand_name  }</p>
+      <p>{ products[3].title }</p>
       </Section>
+
+      <ItemCard products={products}/>
     
     </section>
     
