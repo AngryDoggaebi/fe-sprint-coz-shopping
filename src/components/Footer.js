30fd6b08f9cfc349e63449169108c491b52506be
AngryDoggaebi <aofhoa@gmail.com>
2023-05-26 15:38:49 +0900

    footer css 변경

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,26 @@
+import styled from "styled-components"
+
+let FooterInfo = styled.footer`
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  height: 58px;
+  position : relative;
+  transform : translateY(-100%);
+  border-top: 1px solid;
+  color: rgb(186, 186, 186);
+`
+let FooterLine = styled.p`
+  color: rgb(186, 186, 186);
+`
+
 export default function Footer () {
+  
   return (
 
-    <footer>
-      <p className="footer-line">개인정보 처리방침 | 이용약관</p>
-      <p className="footer-line">All rights reserved @ Codestates</p>
-    </footer>
+    <FooterInfo>
+      <FooterLine>개인정보 처리방침 | 이용약관</FooterLine>
+      <FooterLine>All rights reserved @ Codestates</FooterLine>
+    </FooterInfo>
   )
 }
